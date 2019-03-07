a72944975538d658f6298198e4ff257e4b89e3fe
rakeshmadivi <rakesh.madivi@gmail.com>
2019-03-07 19:38:37 +0530

    Added my options for getopt

--- parse_args.sh
+++ parse_args.sh
@@ -1,30 +1,115 @@
 #!/usr/bin/env bash
 
-# “a” and “arga” have optional arguments with default values.
-# “b” and “argb” have no arguments, acting as sort of a flag.
-# “c” and “argc” have required arguments.
+# Options to be passed.
+: '
+1. rwmixreads
+2. blocksizes
+3. skip_preconditioning
+4. skip_purge
+5. iodepth
+6. numjobs
+7. writepattern
+8. Activerange
+9. maxrounds
+10. disk path
+'
 
-# set an initial value for the flag
-ARG_B=0
+show_help()
+{
+	rwmixwrite=""	#w
+	blocksizes=""	#b
+	skip_preconditioning=""	#c
+	skip_purge=""	#p
+	iodepth=""	#i
+	numjobs=""	#n
+	writepattern=""	#t
+	Activerange=""	#r
+	maxrounds=""	#m
+	diskpath=""	#d
+}
+
+# Arguments to be set
+rwmixwrite=""	#w
+blocksizes=""	#b
+skip_preconditioning=""	#c
+skip_purge=""	#p
+iodepth=""	#i
+numjobs=""	#n
+writepattern=""	#t
+Activerange=""	#r
+maxrounds=""	#m
+diskpath=""	#d
 
 # read the options
-TEMP=`getopt -o a:bc: --long arga::,argb,argc: -n 'test.sh' -- "$@"`
+#TEMP=`getopt -o a:bc: --long arga::,argb,argc: -n 'test.sh' -- "$@"`
+TEMP=`getopt -o w:b:c::p::i:n:t:r:m:d: --long no-preconditioning::,skip-purge::,rwmixwrite:,block-size:,iodepth:,numjobs:,write-pattern:,active-range:,max-rounds:,disk-path: -- "$@"`
 eval set -- "$TEMP"
 
 # extract options and their arguments into variables.
 while true ; do
 	case "$1" in
-		-a|--arga)
+		#1
+		-w|--rwmixwrite)
 			case "$2" in
-				"") shift 2;; #ARG_A='some default value' ; shift 2 ;;
-				*) ARG_A=$2 ; shift 2 ;;
-				#ARG_A=$2 ; shift 22;;
+				"") shift 2;; 
+				*) rwmixwrite=$2; shift 2;;	#w
 			esac ;;
-		-b|--argb) ARG_B=1 ; shift ;;
-		-c|--argc)
+		#2
+		-b|--block-size)
 			case "$2" in
 				"") shift 2 ;;
-				*) ARG_C=$2 ; shift 2 ;;
+				*) blocksizes=$2; shift 2;;	#b
+			esac ;;
+		#3
+		-c|--no-preconditioning)
+			case "$2" in
+				#"") skip_preconditioning='YES'; shift 2;; 
+				"") shift 2 ;;
+				*) skip_preconditioning=$2; shift 2;;	#c
+			esac ;;
+		#4
+		-p|--skip-purge)
+			case "$2" in
+				#"") shift 2;; 
+				#"") skip_purge='YES'; shift 2;;	#p
+				"") shift 2 ;;
+				*) skip_purge=$2; shift 2;;	#p
+			esac ;;
+		#5
+		-i|--iodepth)
+			case "$2" in
+				"") shift 2;; 
+				*) iodepth=$2 ; shift 2;;	#i
+			esac ;;
+		#6
+		-n|--numjobs)
+			case "$2" in
+				"") shift 2;; 
+				*) numjobs=$2; shift 2;; 	#n
+			esac ;;
+		#7
+		-t|--write-pattern)
+			case "$2" in
+				"") shift 2;; 
+				*) writepattern=$2 ; shift 2;;	#t
+			esac ;;
+		#8
+		-r|--active-range)
+			case "$2" in
+				"") shift 2;; 
+				*) activerange=$2;shift 2;;	#r
+			esac ;;
+		#9
+		-m|--max-rounds)
+			case "$2" in
+				"") shift 2;; 
+				*) maxrounds=$2 ; shift 2;;	#m
+			esac ;;
+		#10
+		-d|--disk-path)
+			case "$2" in
+				"") shift 2;; 
+				*) diskpath=$2 ; shift 2;;	#d
 			esac ;;
 		--) shift ; break ;;
 		*) echo "Internal error!" ; exit 1 ;;
@@ -32,6 +117,14 @@ while true ; do
 done
 
 # do something with the variables -- in this case the lamest possible one :-)
-echo "ARG_A = $ARG_A"
-echo "ARG_B = $ARG_B"
-echo "ARG_C = $ARG_C"
+
+echo rwmixwrite=$rwmixwrite	
+echo blocksizes=$blocksizes
+echo skip_preconditioning=$skip_preconditioning
+echo skip_purge=$skip_purge
+echo iodepth=$iodepth
+echo numjobs=$numjobs
+echo writepattern=$writepattern
+echo activerange=$activerange
+echo maxrounds=$maxrounds
+echo diskpath=$diskpath
