824bf16e5f0f216bda61bed6b36d4ad587557e12
rakesh.madivi <rakesh.madivi@gmail.com>
2019-03-13 01:40:42 +0530

    Added elasticsearch

---
+++ elasticsearch/install_required.sh
@@ -0,0 +1,14 @@
+install_logstash()
+{
+	wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+	sudo apt-get install apt-transport-https
+	echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+	sudo apt-get update && sudo apt-get install logstash
+}
+
+logstash_docker()
+{
+	docker pull docker.elastic.co/logstash/logstash:6.6.2
+}
+
+install_logstash
