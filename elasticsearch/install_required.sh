fb189aa0a7e4d16b8ad2ef82a1735e5851c2498e
rakeshmadivi <rakesh.madivi@gmail.com>
2019-03-14 12:44:19 +0530

    Added install_elasticsearch()

--- elasticsearch/install_required.sh
+++ elasticsearch/install_required.sh
@@ -11,4 +11,13 @@ logstash_docker()
 	docker pull docker.elastic.co/logstash/logstash:6.6.2
 }
 
-install_logstash
+install_elasticsearch()
+{
+	wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+	echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+	sudo apt-get update
+	sudo apt-get install elasticsearch
+}
+
+#install_logstash
+install_elasticsearch
